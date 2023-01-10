98b93bf4d4607f1b42e77a4a8ce0ad896c7c29e9
crock3rr <nikitaivah@gmail.com>
2023-01-10 22:06:55 +0500

    создал скрипт во views берущий значения из api hh.ru

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+import pandas as pd
+import requests as req
 
+from django.shortcuts import render
 from app.models import MainPage
 
 
@@ -11,4 +13,22 @@ def main_page(request):
 
 
 def hh_page(request):
-    return render(request, "hh.html")
+    result = []
+    date = '15.12.2022'
+    for page in range(20):
+        path = f'https://api.hh.ru/vacancies?specialization=1&date_from={date}&date_to={date}&page={page}&per_page=100'
+        res = req.get(path).json()
+        [result.append([el['name'], el['snippet']['responsibility'], el['snippet']['requirement'],
+                        el['employer']['name'], el['salary']['from'] if el['salary'] is not None else None,
+                        el['salary']['to'] if el['salary'] is not None else None,
+                        el['salary']['currency'] if el['salary'] is not None else None, el['area']['name'],
+                        el['published_at']]) for el in res['items']]
+
+    frame = pd.DataFrame(result, columns=["name", "description", "key_skills", "employer_name", "salary_from",
+                                          "salary_to", "salary_currency", "area_name", "published_at"]).fillna("")
+    frame = frame[frame.name.str.contains("php") | frame.name.str.contains("пхп") |
+                  frame.name.str.contains("рнр")].sort_values('published_at')[:10]
+    result = {
+        'table': frame.to_html(index=False)
+    }
+    return render(request, "hh.html", context=result)
