c5892b4e3b673b1ae70fb54be0412752d1102869
crock3rr <nikitaivah@gmail.com>
2023-01-10 15:28:53 +0500

    создал вторую страницу "последние вакансии"

--- app/views.py
+++ app/views.py
@@ -8,3 +8,7 @@ def main_page(request):
         'profession': MainPage.objects.get(id=1)
     }
     return render(request, "main.html", context=data)
+
+
+def hh_page(request):
+    return render(request, "hh.html")
--- phpdev/urls.py
+++ phpdev/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import main_page
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_page)
+    path('', main_page),
+    path('hh/', hh_page)
 ]
