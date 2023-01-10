2e978f0e4d57c7d7ab324aee0e2459822e4480ea
crock3rr <nikitaivah@gmail.com>
2023-01-10 13:37:09 +0500

    добавил главную страницу

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class MainPage(models.Model):
+    title = models.CharField('Название профессии', max_length=70)
+    description = models.TextField('Описание профессии')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Вакансия"
+        verbose_name_plural = "Вакансии"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import MainPage
+
+
+def main_page(request):
+    data = {
+        'profession': MainPage.objects.get(id=1)
+    }
+    return render(request, "main.html", context=data)
