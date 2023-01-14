681041c1f85a15677fe2796ba5dc814952aa8a8a
Krayseer <Evgeny.Svalukhin@urfu.me>
2023-01-14 14:54:31 +0500

    перенес логику из views в models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import requests as req
 from django.db import models
 
 
@@ -18,6 +20,28 @@ class Vacancies(models.Model):
 
     def __str__(self):
         return 'Дата'
+
+    @staticmethod
+    def get_result_from_api(date):
+        vacancies = []
+        for index in range(20):
+            path = f'https://api.hh.ru/vacancies?specialization=1&date_from={date}&date_to={date}&page={index}&per_page=100'
+            res = req.get(path).json()
+            for vacancy in res['items']:
+                salary = None
+                if vacancy['salary'] is not None:
+                    general = list(filter(None, [vacancy["salary"]["from"], vacancy["salary"]["to"]]))
+                    salary = f'{"-".join(list(map(str, general)))} {vacancy["salary"]["currency"]}'
+                vacancies.append(
+                    [vacancy['name'], vacancy['snippet']['responsibility'], vacancy['snippet']['requirement'],
+                     vacancy['employer']['name'], salary, vacancy['area']['name'], vacancy['published_at']])
+
+        df = pd.DataFrame(vacancies, columns=["Название вакансии", "Описание вакансии", "Навыки", "Компания", "Оклад",
+                                              "Название региона", "Дата публикации вакансии"]).fillna("")
+        df = df[df['Название вакансии'].str.lower().str.contains("php") |
+                df['Название вакансии'].str.lower().str.contains("пхп") |
+                df['Название вакансии'].str.lower().str.contains("рнр")].sort_values('Дата публикации вакансии')[:10]
+        return df
 
     class Meta:
         verbose_name = "Дата"
--- app/views.py
+++ app/views.py
@@ -14,23 +14,6 @@ def main_page(request):
 
 
 def hh_page(request):
-    vacancies = []
     date = Vacancies.objects.get(id=1).date
-    for index in range(20):
-        path = f'https://api.hh.ru/vacancies?specialization=1&date_from={date}&date_to={date}&page={index}&per_page=100'
-        res = req.get(path).json()
-        for vacancy in res['items']:
-            salary = None
-            if vacancy['salary'] is not None:
-                general = list(filter(None, [vacancy["salary"]["from"], vacancy["salary"]["to"]]))
-                salary = f'{"-".join(list(map(str, general)))} {vacancy["salary"]["currency"]}'
-            vacancies.append([vacancy['name'], vacancy['snippet']['responsibility'], vacancy['snippet']['requirement'],
-                              vacancy['employer']['name'], salary, vacancy['area']['name'], vacancy['published_at']])
-
-    df = pd.DataFrame(vacancies, columns=["Название вакансии", "Описание вакансии", "Навыки", "Компания", "Оклад",
-                                          "Название региона", "Дата публикации вакансии"]).fillna("")
-    df = df[df['Название вакансии'].str.lower().str.contains("php") |
-            df['Название вакансии'].str.lower().str.contains("пхп") |
-            df['Название вакансии'].str.lower().str.contains("рнр")].sort_values('Дата публикации вакансии')[:10]
-
-    return render(request, "hh.html", context={'table': df.to_html(index=False)})
+    result = Vacancies.get_result_from_api(date)
+    return render(request, "hh.html", context={'table': result.to_html(index=False)})
